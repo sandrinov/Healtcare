067de41e5481cfdaab00ce03315be4b65bb2f57a
sandro.vecchiarelli <sandro.vecchiarelli@pomiager.com>
2021-03-10 10:59:21 +0100

    Add project files.

---
+++ HealtCareNetCore3/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HealtCareNetCore3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
