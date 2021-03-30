eadcf1e901aad6fab32f7fc350cdb1845baeeeaf
sandro.vecchiarelli <sandro.vecchiarelli@pomiager.com>
2021-03-30 18:06:24 +0200

    30032021_Commit

---
+++ AppointmentsManagerShared/Call.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AppointmentsManagerShared
+{
+    public class Call
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Provider { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public string Note { get; set; }
+    }
+}
